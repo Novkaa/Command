3658fdf78c987b5509e05273d3e44802103f987a
Nova Зотов <zotovnova@gmail.com>
2024-04-16 20:56:48 +0300

    Изначальная странница проекта

--- CommandLabYOY/Form1.Designer.cs
+++ CommandLabYOY/Form1.Designer.cs
@@ -28,12 +28,458 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            pictureBox5 = new PictureBox();
+            pictureBox6 = new PictureBox();
+            pictureBox7 = new PictureBox();
+            pictureBox8 = new PictureBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            textBox7 = new TextBox();
+            textBox8 = new TextBox();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 40F);
+            label1.Location = new Point(388, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(181, 72);
+            label1.TabIndex = 0;
+            label1.Text = "Меню";
+            label1.Click += label1_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.GrayText;
+            button1.BackgroundImageLayout = ImageLayout.None;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Segoe UI", 15F);
+            button1.ForeColor = SystemColors.ControlText;
+            button1.Location = new Point(12, 108);
+            button1.Name = "button1";
+            button1.Size = new Size(195, 50);
+            button1.TabIndex = 1;
+            button1.Text = "Акции";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.GrayText;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Font = new Font("Segoe UI", 15F);
+            button2.Location = new Point(12, 184);
+            button2.Name = "button2";
+            button2.Size = new Size(195, 50);
+            button2.TabIndex = 2;
+            button2.Text = "Новкинки";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.GrayText;
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.Font = new Font("Segoe UI", 15F);
+            button3.Location = new Point(12, 260);
+            button3.Name = "button3";
+            button3.Size = new Size(195, 50);
+            button3.TabIndex = 3;
+            button3.Text = "Блюда";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.GrayText;
+            button4.FlatStyle = FlatStyle.Popup;
+            button4.Font = new Font("Segoe UI", 15F);
+            button4.Location = new Point(12, 328);
+            button4.Name = "button4";
+            button4.Size = new Size(195, 50);
+            button4.TabIndex = 4;
+            button4.Text = "Напитки";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.GrayText;
+            button5.FlatStyle = FlatStyle.Popup;
+            button5.Font = new Font("Segoe UI", 15F);
+            button5.Location = new Point(12, 396);
+            button5.Name = "button5";
+            button5.Size = new Size(195, 50);
+            button5.TabIndex = 5;
+            button5.Text = "Для детей";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.LimeGreen;
+            button6.FlatStyle = FlatStyle.Popup;
+            button6.Font = new Font("Segoe UI", 15F);
+            button6.Location = new Point(38, 477);
+            button6.Name = "button6";
+            button6.Size = new Size(141, 50);
+            button6.TabIndex = 6;
+            button6.Text = "Корзина";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = SystemColors.ControlDarkDark;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Location = new Point(226, 108);
+            button7.Name = "button7";
+            button7.Size = new Size(25, 475);
+            button7.TabIndex = 7;
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = SystemColors.ControlDarkDark;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Location = new Point(226, 108);
+            button8.Name = "button8";
+            button8.Size = new Size(847, 20);
+            button8.TabIndex = 8;
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(272, 143);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(146, 152);
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(471, 143);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(146, 152);
+            pictureBox2.TabIndex = 10;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Location = new Point(671, 143);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(146, 152);
+            pictureBox3.TabIndex = 11;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Location = new Point(873, 143);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(146, 152);
+            pictureBox4.TabIndex = 12;
+            pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Location = new Point(272, 375);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(146, 152);
+            pictureBox5.TabIndex = 13;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Location = new Point(471, 375);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(146, 152);
+            pictureBox6.TabIndex = 14;
+            pictureBox6.TabStop = false;
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Location = new Point(671, 375);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(146, 152);
+            pictureBox7.TabIndex = 15;
+            pictureBox7.TabStop = false;
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Location = new Point(873, 375);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(146, 152);
+            pictureBox8.TabIndex = 16;
+            pictureBox8.TabStop = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(272, 301);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(115, 23);
+            textBox1.TabIndex = 17;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(471, 301);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(115, 23);
+            textBox2.TabIndex = 18;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(671, 301);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(115, 23);
+            textBox3.TabIndex = 19;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(873, 301);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(115, 23);
+            textBox4.TabIndex = 20;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(272, 533);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(115, 23);
+            textBox5.TabIndex = 21;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(471, 533);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(115, 23);
+            textBox6.TabIndex = 22;
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(671, 533);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(115, 23);
+            textBox7.TabIndex = 23;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(873, 533);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(115, 23);
+            textBox8.TabIndex = 24;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.Green;
+            button9.FlatStyle = FlatStyle.Popup;
+            button9.Font = new Font("Segoe UI", 10F);
+            button9.Location = new Point(393, 301);
+            button9.Name = "button9";
+            button9.Size = new Size(32, 22);
+            button9.TabIndex = 25;
+            button9.Text = "+";
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.Green;
+            button10.FlatStyle = FlatStyle.Popup;
+            button10.Font = new Font("Segoe UI", 10F);
+            button10.Location = new Point(592, 302);
+            button10.Name = "button10";
+            button10.Size = new Size(32, 22);
+            button10.TabIndex = 26;
+            button10.Text = "+";
+            button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.Green;
+            button11.FlatStyle = FlatStyle.Popup;
+            button11.Font = new Font("Segoe UI", 10F);
+            button11.Location = new Point(792, 302);
+            button11.Name = "button11";
+            button11.Size = new Size(32, 22);
+            button11.TabIndex = 27;
+            button11.Text = "+";
+            button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.Green;
+            button12.FlatStyle = FlatStyle.Popup;
+            button12.Font = new Font("Segoe UI", 10F);
+            button12.Location = new Point(994, 302);
+            button12.Name = "button12";
+            button12.Size = new Size(32, 22);
+            button12.TabIndex = 28;
+            button12.Text = "+";
+            button12.UseVisualStyleBackColor = false;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.Green;
+            button13.FlatStyle = FlatStyle.Popup;
+            button13.Font = new Font("Segoe UI", 10F);
+            button13.Location = new Point(393, 534);
+            button13.Name = "button13";
+            button13.Size = new Size(32, 22);
+            button13.TabIndex = 29;
+            button13.Text = "+";
+            button13.UseVisualStyleBackColor = false;
+            // 
+            // button14
+            // 
+            button14.BackColor = Color.Green;
+            button14.FlatStyle = FlatStyle.Popup;
+            button14.Font = new Font("Segoe UI", 10F);
+            button14.Location = new Point(592, 534);
+            button14.Name = "button14";
+            button14.Size = new Size(32, 22);
+            button14.TabIndex = 30;
+            button14.Text = "+";
+            button14.UseVisualStyleBackColor = false;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.Green;
+            button15.FlatStyle = FlatStyle.Popup;
+            button15.Font = new Font("Segoe UI", 10F);
+            button15.Location = new Point(792, 531);
+            button15.Name = "button15";
+            button15.Size = new Size(32, 22);
+            button15.TabIndex = 31;
+            button15.Text = "+";
+            button15.UseVisualStyleBackColor = false;
+            // 
+            // button16
+            // 
+            button16.BackColor = Color.Green;
+            button16.FlatStyle = FlatStyle.Popup;
+            button16.Font = new Font("Segoe UI", 10F);
+            button16.Location = new Point(994, 533);
+            button16.Name = "button16";
+            button16.Size = new Size(32, 22);
+            button16.TabIndex = 32;
+            button16.Text = "+";
+            button16.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.InactiveCaption;
+            ClientSize = new Size(1031, 571);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(textBox8);
+            Controls.Add(textBox7);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(pictureBox8);
+            Controls.Add(pictureBox7);
+            Controls.Add(pictureBox6);
+            Controls.Add(pictureBox5);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox6;
+        private PictureBox pictureBox7;
+        private PictureBox pictureBox8;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private TextBox textBox7;
+        private TextBox textBox8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
     }
 }
