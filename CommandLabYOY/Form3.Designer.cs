e158fa392ab87e278bab68f3e589adefd8064df5
Елизавета Урсаке <lizzxq@mail.ru>
2024-04-22 23:02:04 +0300

    1

--- CommandLabYOY/Form3.Designer.cs
+++ CommandLabYOY/Form3.Designer.cs
@@ -45,28 +45,29 @@
             // 
             label1.BackColor = SystemColors.ControlLight;
             label1.BorderStyle = BorderStyle.Fixed3D;
-            label1.Font = new Font("Segoe UI", 50F);
-            label1.Location = new Point(606, 9);
+            label1.Font = new Font("Segoe UI", 50F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(693, 12);
             label1.Name = "label1";
-            label1.Size = new Size(273, 109);
+            label1.Size = new Size(312, 145);
             label1.TabIndex = 0;
             label1.Text = "Оплата";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(542, 169);
+            textBox1.Location = new Point(619, 225);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(425, 23);
+            textBox1.Size = new Size(485, 27);
             textBox1.TabIndex = 1;
             // 
             // label2
             // 
             label2.BackColor = SystemColors.ControlLight;
             label2.BorderStyle = BorderStyle.Fixed3D;
-            label2.Font = new Font("Segoe UI", 10F);
-            label2.Location = new Point(542, 139);
+            label2.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(619, 185);
             label2.Name = "label2";
-            label2.Size = new Size(267, 27);
+            label2.Size = new Size(305, 36);
             label2.TabIndex = 6;
             label2.Text = "Адрес электронной почты";
             // 
@@ -74,10 +75,10 @@
             // 
             label3.BackColor = SystemColors.ControlLight;
             label3.BorderStyle = BorderStyle.Fixed3D;
-            label3.Font = new Font("Segoe UI", 10F);
-            label3.Location = new Point(542, 227);
+            label3.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(619, 303);
             label3.Name = "label3";
-            label3.Size = new Size(117, 27);
+            label3.Size = new Size(134, 36);
             label3.TabIndex = 7;
             label3.Text = "Номер карты";
             // 
@@ -85,10 +86,10 @@
             // 
             label4.BackColor = SystemColors.ControlLight;
             label4.BorderStyle = BorderStyle.Fixed3D;
-            label4.Font = new Font("Segoe UI", 10F);
-            label4.Location = new Point(542, 283);
+            label4.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(619, 377);
             label4.Name = "label4";
-            label4.Size = new Size(125, 27);
+            label4.Size = new Size(143, 36);
             label4.TabIndex = 8;
             label4.Text = "Действителен до";
             // 
@@ -96,64 +97,71 @@
             // 
             label5.BackColor = SystemColors.ControlLight;
             label5.BorderStyle = BorderStyle.Fixed3D;
-            label5.Font = new Font("Segoe UI", 10F);
-            label5.Location = new Point(710, 283);
+            label5.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(811, 377);
             label5.Name = "label5";
-            label5.Size = new Size(64, 27);
+            label5.Size = new Size(73, 36);
             label5.TabIndex = 9;
             label5.Text = "CVC";
             // 
             // maskedTextBox1
             // 
-            maskedTextBox1.Location = new Point(542, 313);
+            maskedTextBox1.Location = new Point(619, 417);
+            maskedTextBox1.Margin = new Padding(3, 4, 3, 4);
             maskedTextBox1.Name = "maskedTextBox1";
-            maskedTextBox1.Size = new Size(125, 23);
+            maskedTextBox1.Size = new Size(142, 27);
             maskedTextBox1.TabIndex = 10;
             // 
             // maskedTextBox2
             // 
-            maskedTextBox2.Location = new Point(542, 257);
+            maskedTextBox2.Location = new Point(619, 343);
+            maskedTextBox2.Margin = new Padding(3, 4, 3, 4);
             maskedTextBox2.Name = "maskedTextBox2";
-            maskedTextBox2.Size = new Size(284, 23);
+            maskedTextBox2.Size = new Size(324, 27);
             maskedTextBox2.TabIndex = 11;
             // 
             // maskedTextBox3
             // 
-            maskedTextBox3.Location = new Point(710, 313);
+            maskedTextBox3.Location = new Point(811, 417);
+            maskedTextBox3.Margin = new Padding(3, 4, 3, 4);
             maskedTextBox3.Name = "maskedTextBox3";
             maskedTextBox3.PasswordChar = '*';
-            maskedTextBox3.Size = new Size(64, 23);
+            maskedTextBox3.Size = new Size(73, 27);
             maskedTextBox3.TabIndex = 12;
             // 
             // button2
             // 
             button2.BackColor = Color.Red;
-            button2.Font = new Font("Segoe UI", 15F);
-            button2.Location = new Point(12, 9);
+            button2.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.Location = new Point(14, 12);
+            button2.Margin = new Padding(3, 4, 3, 4);
             button2.Name = "button2";
-            button2.Size = new Size(206, 77);
+            button2.Size = new Size(235, 103);
             button2.TabIndex = 14;
             button2.Text = "Вернуться в корзину";
             button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click_1;
             // 
             // button3
             // 
             button3.BackColor = Color.FromArgb(128, 255, 128);
             button3.Cursor = Cursors.Hand;
             button3.FlatStyle = FlatStyle.Popup;
-            button3.Font = new Font("Segoe UI", 15F);
-            button3.Location = new Point(574, 474);
+            button3.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.Location = new Point(656, 632);
+            button3.Margin = new Padding(3, 4, 3, 4);
             button3.Name = "button3";
-            button3.Size = new Size(367, 50);
+            button3.Size = new Size(419, 67);
             button3.TabIndex = 15;
             button3.Text = "Оплатить";
             button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
             // 
             // Form3
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1457, 648);
+            ClientSize = new Size(1665, 864);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(maskedTextBox3);
@@ -165,6 +173,7 @@
             Controls.Add(label2);
             Controls.Add(textBox1);
             Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "Form3";
             Text = "4242";
             Load += Form3_Load;
--- CommandLabYOY/Form3.cs
+++ CommandLabYOY/Form3.cs
@@ -19,17 +19,27 @@ namespace CommandLabYOY
 
         private void Form3_Load(object sender, EventArgs e)
         {
-            
+
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
-            
+
         }
 
-        private void button2_Click(object sender, EventArgs e)
+
+
+        private void button2_Click_1(object sender, EventArgs e)
         {
-           this.Close();
+            Close();
+
+
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+
         }
     }
 }
